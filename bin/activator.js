4912c4852c7ecd27469f6babf37f6f707c8626cd
Juan Rudin <juan.rudin@drivedominion.com>
2021-03-19 11:57:18 -0400

    Changes. Ngrok, EventWebHook.

--- bin/activator.js
+++ bin/activator.js
@@ -1,13 +1,17 @@
 const inquirer = require('inquirer')
 //const { exec } = require("child_process");
 const { exec, spawn } = require("child_process");
+const request = require('request');
+const fs = require('fs');
 
 inquirer.registerPrompt('autosubmit', require('inquirer-autosubmit-prompt'));
 
 exports.activator = function() {
   console.log("I am activator");
   console.log( "1: Run DemoData" );
-  console.log( "2: Create Event Grid Webhooks")
+  console.log( "2: Run ngrok");
+  console.log( "3: Create Event Grid Webhooks");
+  console.log( "4: FixLocalPsFile");
   //console.log( "0: Go back" );
 
   var questions = [
@@ -32,12 +36,17 @@ exports.activator = function() {
         });
         break;
       case "2":
-        const app2 = "C:\\DevFiles\\Code\\Equity\\Local\\Tools\\CreateEventGridSubscriptions.local.ps1"
-        var child = spawn("powershell.exe",[app2]);
-        child.stdout.on("data",function(data){
+        const app3 = "ngrok";
+        const args3 = ["http", "-host-header=rewrite", "local.activateotto.com:80"];
+        var child = spawn(app3, args3, {detached:true, stdio: 'ignore'}).unref();
+        break;
+      case "3":
+        const psCommand = "cd D:/Git/Equity/Tools; ./testing.ps1";
+        var child = spawn("powershell.exe", [psCommand]);
+        child.stdout.on("data", function(data){
           console.log("Powershell Data: " + data);
         });
-        child.stderr.on("data",function(data){
+        child.stderr.on("data", function(data){
           console.log("Powershell Errors: " + data);
         });
         child.on("exit",function(){
@@ -45,6 +54,25 @@ exports.activator = function() {
         });
         child.stdin.end(); //end input
         break;
+      case "4":
+        request("http://localhost:4040/api/tunnels", { json: true }, (err, res, body) => {
+          if (err) return console.log(err);
+          
+          let httpsTunnel = body.tunnels[0].public_url;
+          let httpTunnel = body.tunnels[1].public_url;
+          let command4 = "./CreateEventGridSubscriptions.base.ps1 " + 
+            "-EndpointRoot \"" + httpsTunnel + "\" " +
+            "-SubscriptionNamePrefix \"JR-\" " + 
+            "-ResourceGroup \"Ottobase-General\" " + 
+            "-SubscriptionId \"e1f843ce-5bac-42bd-b3c2-a9ea02672dab\" " + 
+            "-TenantId \"1ddf5542-6a20-4020-bb72-0d757c795785\" " + 
+            "-TopicName \"ma-dev-eventgridtopic\"";
+          fs.writeFile('D:/Git/Equity/Tools/testing.ps1', command4, function (err) {
+            if (err) console.log(err);
+          });
+        });
+        break;
+      
       default:
         console.log('say what?');
     }
