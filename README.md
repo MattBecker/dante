dea12f3f8d72e01c31587762c1a18557356ae4df
Matt Becker <matt.becker@drivedominion.com>
2021-06-25 13:27:49 -0400

    Failed attempt at getting r# cli working with emailbuilder

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ DANTE (Developers Are Needing Tools Everywhere)
 
 Create a .dante file (C:\Users\{yourname}\)
 {
-  "activatorRoot": "C:\\DevFiles\\Code\\Equity\\Local",
+  "activatorRoot": "C:\\DevFiles\\Code\\Equity\\Local\\",
+  "emailBuilderRoot": "C:\\DevFiles\\Code\\EmailBuilder\\Local\\",
   "initials": "MB",
-  "ngrokRoot": "C:\\DevFiles\\ngrok-stable-windows-amd64"
+  "ngrokRoot": "C:\\DevFiles\\ngrok-stable-windows-amd64\\",
+  "resharperRoot": "C:\\DevFiles\\JetBrains.ReSharper.CommandLineTools.2020.2.4\\"
 }
--- bin/emailbuilder.js
+++ bin/emailbuilder.js
@@ -1,7 +1,9 @@
 const inquirer = require('inquirer')
-const { exec, spawn } = require("child_process");
+const { exec, spawn, spawnSync } = require("child_process");
 const request = require('request');
 const fs = require('fs');
+const os = require('os');
+
 var settings = require('user-settings').file('.dante');
 
 inquirer.registerPrompt('autosubmit', require('inquirer-autosubmit-prompt'));
@@ -29,17 +31,100 @@ exports.init = function() {
 
     switch(ans) {
       case "1":
-        const args1 = [emailBuilderRoot + "\\EmailBuilder\\EmailBuilder.sln"];
+        var filesChanged = getFilesChanged();
+
+        console.log('fc=' + filesChanged);
+
+        //var filesChangedWithSemis = filesChanged.split(os.EOL).join(';');
+        var filesChangedWithSemis = filesChanged.replace(os.EOL, ';');
+        //var filesChangedWithSemis = filesChanged.join(';');
+
+        console.log('fcws=' + filesChangedWithSemis);
+
+        const args1 = [emailBuilderRoot + "\\EmailBuilder\\EmailBuilder.sln", "--include", filesChangedWithSemis];
         var child = spawn(resharperCliApp, args1);
         child.stdout.on('data', function(msg){
           console.log(msg.toString())
         });
-      break;
 
         break;
 
       default:
         console.log('say what now?');
     }
-  })
-}
+  });
+};
+
+var getFilesChanged = function() {
+  //console.log('getFilesChanged');
+  var emailBuilderRoot = settings.get('emailBuilderRoot');
+
+  //var ngrokRoot = settings.get('ngrokRoot');
+  const app = "git";
+  const args = ["diff", "--name-only"];
+  //exec('cd ' + emailBuilderRoot + '; C:\\Program%20Files\\Git\\cmd\\git.exe diff --name-only', (err, stdout, stderr) => {
+  //exec("cd " + emailBuilderRoot + "; C:\\Progra~1\\Git\\cmd\\git.exe diff --name-only", (err, stdout, stderr) => {
+//    exec('C:\\Program%20Files\\Git\\cmd\\git.exe diff --name-only', (err, stdout, stderr) => {
+  //exec('C:\\Program%20Files\\Git\\cmd\\git.exe diff --name-only', (err, stdout, stderr) => {
+
+  const psCommand = "cd " + emailBuilderRoot + "; C:\\Progra~1\\Git\\cmd\\git.exe diff --name-only";
+  var output = runPsCommandSync(psCommand);
+
+  // parse the output of "git diff"
+    //const diffs = parseDiff(stdout);
+//console.log(output);
+  // array of diff objects
+  //console.log('err=' + err);
+  //console.log('stdout=' + stdout);
+  //console.log('stderr=' + stderr);
+
+  //});
+  return output;
+};
+
+var runPsCommandSync = function(psCommand){
+  //console.log("Running PS command...", psCommand);
+  let output = "";
+  let errors = "";
+  var retval = "";
+
+  //var child = spawnSync("powershell.exe", [psCommand], {
+    //stdio: [process.stdin, process.stdout, process.stderr]
+//});
+
+//  return child.output;
+
+  var child = spawnSync("powershell.exe", [psCommand]);
+
+return child.stdout.toString();
+};
+
+
+var runPsCommand = function(psCommand){
+  //console.log("Running PS command...", psCommand);
+  let output = "";
+  let errors = "";
+  var retval = "";
+
+  var child = spawn("powershell.exe", [psCommand]);
+  child.stdout.on("data", data => {
+    //process.stdout.write(".");
+    output += data;
+  });
+  child.stderr.on("data", data => {
+    process.stdout.write(".");
+    errors += data;
+  });
+  child.on("exit", () => {
+    console.log("");
+    //console.log("Output: ", output);
+    //console.log("Errors: ", errors);
+    retval = output;
+    //return output;
+  });
+  child.stdin.end();
+  //console.log("PS command finished..");
+  console.log("Retval: ", retval);
+
+  return retval;
+};
